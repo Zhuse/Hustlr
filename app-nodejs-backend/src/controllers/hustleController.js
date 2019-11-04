3c30f00fc9b7e08d87cdb1bc753349210c7d8fd5
Jeff Yang <jeffjieyang@gmail.com>
2019-11-03 20:03:10 -0800

    fix n+1 query issue

--- app-nodejs-backend/src/controllers/hustleController.js
+++ app-nodejs-backend/src/controllers/hustleController.js
@@ -1,6 +1,6 @@
 const Hustle = require("../models/hustle");
-const User = require("../models/user");
 const Bid = require("../models/bid");
+const matchService = require("../services/matchService");
 const _ = require("lodash");
 
 exports.create = function (req, res) {
@@ -39,57 +39,14 @@ exports.findById = function (req, res) {
         });
 };
 
-// TODO break logic out
 exports.findMatches = function (req, res) {
-    User.findById(req.params.userId)
-        .then((result) => {
-        // TODO err case
-            const currUser = result;
-            const userScore = currUser.properties.score;
-            const preferredCategories = currUser.properties.preferredCategories;
+    const matches = matchService.findMatches(req.params.userId);
 
-            Hustle.find({ status: "posted", providerId: { $ne: req.params.userId } }).populate("bids")
-                .then((results) => {
-                    const providerIds = results.map((result) => { return result.providerId; });
-
-                    User.find({ _id: { $in: providerIds } })
-                        .then((providers) => {
-                            const validProviders = providers.filter((provider) => {
-                                return Math.abs(provider.properties.score - userScore) < 10;
-                            });
-
-                            const validProviderIds = validProviders.map((provider) => {
-                                return provider._id.toString();
-                            });
-
-                            const filtered = [];
-
-                            results.forEach((hustle) => {
-                                if (validProviderIds.includes(hustle.providerId.toString()) &&
-                            preferredCategories.includes(hustle.category)) {
-                                    filtered.push(hustle);
-                                }
-                            });
-
-                            const preSend = {
-                                userId: req.params.userId,
-                                properties: {
-                                    hustles: filtered
-                                }
-                            };
-                            return res.status(200).send(preSend);
-                        })
-                        .catch((err) => {
-                            return res.status(500).send(err);
-                        });
-                })
-                .catch((err) => {
-                    return res.status(400).send(err);
-                });
-        })
-        .catch((err) => {
-            return res.status(400).send(err);
-        });
+    matches.then((data) => {
+        return res.status(200).send(data);
+    }, (err) => {
+        return res.status(500).send(err);
+    });
 };
 
 exports.update = function (req, res) {
---
+++ app-nodejs-backend/src/services/matchService.js
@@ -0,0 +1,38 @@
+const Hustle = require("../models/hustle");
+const User = require("../models/user");
+
+module.exports.findMatches = function (userId) {
+    return new Promise((resolve, reject) => {
+        User.findById(userId)
+            .then((result) => {
+                const currUser = result;
+                const userScore = currUser.properties.score;
+                const preferredCategories = currUser.properties.preferredCategories;
+
+                Hustle.find({ status: "posted", providerId: { $ne: userId } })
+                    .populate("bids")
+                    .populate("providerId")
+                    .then((results) => {
+                        const validHustles = results.filter((hustle) => {
+                            return preferredCategories.includes(hustle.category);
+                        }).filter((hustle) => {
+                            return Math.abs(hustle.providerId.properties.score - userScore) < 10;
+                        });
+
+                        const preSend = {
+                            userId: userId,
+                            properties: {
+                                hustles: validHustles
+                            }
+                        };
+                        resolve(preSend);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+};
